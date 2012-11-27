ce56fa76647cce863795534ba369fd2b2bd44c3f
pbs\benjambe <benny.benjamin@cba.com.au>
2012-11-27 15:28:46 +1100

    TFS Api stubs

---
+++ TeamCityBuildChanges/ExternalApi/TFS/TfsApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamCityBuildChanges.ExternalApi.TFS
+{
+    public class TfsApi
+    {
+        private string _connectionUri;
+
+        public TfsApi(string connectionUri)
+        {
+            _connectionUri = connectionUri;
+        }
+
+
+
+        internal TfsWorkItem GetWorkItem(int workItemId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeamCityBuildChanges/ExternalApi/TFS/TfsWorkItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamCityBuildChanges.ExternalApi.TFS
+{
+    public class TfsWorkItem
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public string Type { get; set; }
+        public string State { get; set; }
+        public int? ParentId { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public List<string> HistoryComments { get; set; }
+
+        public IEnumerable<int> ChildrenIds { get; set; }
+    }
+}
