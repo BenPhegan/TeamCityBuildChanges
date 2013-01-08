46b264b978ccd8849a09c5360d78526945982ad6
AU\chapmast <chapmast@cba.com.au>
2013-01-09 10:15:52 +1100

    CINDER-873 Update command argument documentation, minor formatting updates (thanks, ReSharper)

--- TeamCityBuildChanges/Commands/AggregateBuildDelta.cs
+++ TeamCityBuildChanges/Commands/AggregateBuildDelta.cs
@@ -19,7 +19,7 @@ namespace TeamCityBuildChanges.Commands
         {
             IsCommand("aggregatebuilddelta", "Provides a set of changes between two specific versions of a build type.");
             Options.Add("rb=|referencebuild=", "Reference build to query resolved version deltas from", s => _referenceBuild = s);
-            Options.Add("f|from=", "Build number to start checking from", x => _from = x);
+            Options.Add("f|from=", "Build number to start checking from (optional - detects the last successful build number if omitted)", x => _from = x);
             Options.Add("t|to=", "The build to check the delta change to", x => _to = x);
 
             Options.Add("zerochangescomment=", "If there are no changes detected, add the provided comment rather than leave it null", x => _zeroChangesComment = x);
@@ -33,7 +33,7 @@ namespace TeamCityBuildChanges.Commands
             var resolver = new AggregateBuildDeltaResolver(api, CreateExternalIssueResolvers());
             ChangeManifest = resolver.CreateChangeManifest(BuildName, BuildType, _referenceBuild, _from, _to, ProjectName);
 
-            OutputChanges(CreateOutputRenderers(),new List<Action<string>>()
+            OutputChanges(CreateOutputRenderers(), new List<Action<string>>()
                 {
                     Console.Write,
                     a =>
