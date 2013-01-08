ece5d40d3e2f0baf7c7f72407cd026692d26a864
AU\chapmast <chapmast@cba.com.au>
2013-01-09 10:16:53 +1100

    CINDER-873 Update command argument documentation, minor formatting updates (thanks, ReSharper)

--- TeamCityBuildChanges/Commands/AggregateBuildDelta.cs
+++ TeamCityBuildChanges/Commands/AggregateBuildDelta.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using TeamCityBuildChanges.ExternalApi.TeamCity;
-using TeamCityBuildChanges.IssueDetailResolvers;
-using TeamCityBuildChanges.Output;
 
 namespace TeamCityBuildChanges.Commands
 {
-    class AggregateBuildDelta : TeamCityCommandBase
+    internal class AggregateBuildDelta : TeamCityCommandBase
     {
         private string _from;
-        private string _to;
         private string _referenceBuild;
+        private string _to;
         private string _zeroChangesComment;
 
         public AggregateBuildDelta()
@@ -21,7 +18,6 @@ namespace TeamCityBuildChanges.Commands
             Options.Add("rb=|referencebuild=", "Reference build to query resolved version deltas from", s => _referenceBuild = s);
             Options.Add("f|from=", "Build number to start checking from (optional - detects the last successful build number if omitted)", x => _from = x);
             Options.Add("t|to=", "The build to check the delta change to", x => _to = x);
-
             Options.Add("zerochangescomment=", "If there are no changes detected, add the provided comment rather than leave it null", x => _zeroChangesComment = x);
             SkipsCommandSummaryBeforeRunning();
         }
@@ -33,15 +29,11 @@ namespace TeamCityBuildChanges.Commands
             var resolver = new AggregateBuildDeltaResolver(api, CreateExternalIssueResolvers());
             ChangeManifest = resolver.CreateChangeManifest(BuildName, BuildType, _referenceBuild, _from, _to, ProjectName);
 
-            OutputChanges(CreateOutputRenderers(), new List<Action<string>>()
+            OutputChanges(CreateOutputRenderers(), new List<Action<string>> {Console.Write, a =>
                 {
-                    Console.Write,
-                    a =>
-                        {
-                            if (!string.IsNullOrEmpty(OutputFileName))
-                                File.WriteAllText(OutputFileName, a);
-                        } 
-                });
+                    if (!string.IsNullOrEmpty(OutputFileName))
+                        File.WriteAllText(OutputFileName, a);
+                }});
             return 0;
         }
     }
