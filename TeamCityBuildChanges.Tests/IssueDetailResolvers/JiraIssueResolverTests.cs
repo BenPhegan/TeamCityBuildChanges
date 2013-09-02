ed64f89ef200ae56aea32585345d995e5a4f6324
Ben Phegan <benphegan@gmail.com>
2013-09-02 16:59:06 +1000

    Fix for non-Jira keys being used to query Jira instances for issue details.

--- TeamCityBuildChanges.Tests/IssueDetailResolvers/JiraIssueResolverTests.cs
+++ TeamCityBuildChanges.Tests/IssueDetailResolvers/JiraIssueResolverTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
 using FakeItEasy;
 using NUnit.Framework;
 using TeamCityBuildChanges.ExternalApi.Jira;
@@ -14,7 +14,7 @@ namespace TeamCityBuildChanges.Tests.IssueDetailResolvers
     public class JiraIssueResolverTests
     {
         [Test]
-        public void Test()
+        public void PurelyNumericIssueNumbersAreNotQueried()
         {
             var mockApi = A.Fake<IJiraApi>();
             var changes = new List<Issue>
@@ -27,7 +27,52 @@ namespace TeamCityBuildChanges.Tests.IssueDetailResolvers
             var results = resolver.GetDetails(changes);
             Assert.AreEqual(0, results.Count());
             A.CallTo(() => mockApi.GetJiraIssue(A<string>.Ignored)).MustNotHaveHappened();
+        }
 
+        [Test]
+        public void CaseDoesNotMatterForCorrectJiraIssues()
+        {
+            var mockApi = A.Fake<IJiraApi>();
+            A.CallTo(() => mockApi.GetJiraIssue(A<string>.That.IsEqualTo("jira-1"))).Returns(GetJiraRootObject("JIRA-1"));
+            A.CallTo(() => mockApi.GetJiraIssue(A<string>.That.IsEqualTo("JIRA-2"))).Returns(GetJiraRootObject("JIRA-2"));
+            A.CallTo(() => mockApi.GetJiraIssue(A<string>.That.IsEqualTo("JirA-3"))).Returns(GetJiraRootObject("JIRA-3"));
+
+            var changes = new List<Issue>
+                {
+                    new Issue {Id = "1", Url = "http://qwerty.io/1"},
+                    new Issue {Id = "jira-1", Url = "http://qwerty.io/2"},
+                    new Issue {Id = "JIRA-2", Url = "http://qwerty.io/2"},
+                    new Issue {Id = "JirA-3", Url = "http://qwerty.io/2"},
+                };
+
+            var resolver = new JiraIssueResolver(mockApi);
+            var results = resolver.GetDetails(changes);
+            Assert.AreEqual(3, results.Count());
+            A.CallTo(() => mockApi.GetJiraIssue(A<string>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(3));
+        }
+
+        private RootObject GetJiraRootObject(string jiraId)
+        {
+            return new RootObject
+                {
+                    Id = jiraId,
+                    Key = jiraId,
+                    Fields = new Fields
+                        {
+                            Status = new Status2
+                                {
+                                    Name = "Resolved"
+                                },
+                            Created = DateTime.Today.ToString(CultureInfo.InvariantCulture),
+                            Summary = "Summary",
+                            Description = "Description",
+                            Issuetype = new Issuetype
+                                {
+                                    Name = "Bug"
+                                }
+
+                        }
+                };
         }
     }
 }
