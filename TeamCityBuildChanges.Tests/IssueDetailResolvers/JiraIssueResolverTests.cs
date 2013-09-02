fa9d6a8940fc5786d7881463ef6b89f21dfac0af
Ben Phegan <benphegan@gmail.com>
2013-09-02 16:34:31 +1000

    Breaking test for Jira being queried for issues that could never resolve to Jira issue types.

---
+++ TeamCityBuildChanges.Tests/IssueDetailResolvers/JiraIssueResolverTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FakeItEasy;
+using NUnit.Framework;
+using TeamCityBuildChanges.ExternalApi.Jira;
+using TeamCityBuildChanges.ExternalApi.TeamCity;
+using TeamCityBuildChanges.IssueDetailResolvers;
+
+namespace TeamCityBuildChanges.Tests.IssueDetailResolvers
+{
+    [TestFixture]
+    public class JiraIssueResolverTests
+    {
+        [Test]
+        public void Test()
+        {
+            var mockApi = A.Fake<IJiraApi>();
+            var changes = new List<Issue>
+                {
+                    new Issue {Id = "1", Url = "http://qwerty.io/1"},
+                    new Issue {Id = "2", Url = "http://qwerty.io/2"},
+                };
+
+            var resolver = new JiraIssueResolver(mockApi);
+            var results = resolver.GetDetails(changes);
+            Assert.AreEqual(0, results.Count());
+            A.CallTo(() => mockApi.GetJiraIssue(A<string>.Ignored)).MustNotHaveHappened();
+
+        }
+    }
+}
