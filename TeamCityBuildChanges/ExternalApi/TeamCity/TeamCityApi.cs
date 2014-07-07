5dbd059d843cb7bd10072f3a364f2d43d585b897
Dipesh Chauhan <dipesh@chauhan.net.au>
2014-07-07 15:07:58 +1000

    Removed commented debug code.

--- TeamCityBuildChanges/ExternalApi/TeamCity/TeamCityApi.cs
+++ TeamCityBuildChanges/ExternalApi/TeamCity/TeamCityApi.cs
@@ -441,9 +441,6 @@ namespace TeamCityBuildChanges.ExternalApi.TeamCity
         public static DateTime ConvertToDateTime(string value)
         {
             return DateTime.ParseExact(value, "yyyyMMdd'T'HHmmsszzz", CultureInfo.InvariantCulture);
-            //Values come in looking like this: 20121022T215947+1100
-            //var splitString = value.Split("+-".ToCharArray()).ToList();
-            //return DateTime.ParseExact(splitString[0], "yyyyMMddTHHmmss", CultureInfo.CurrentCulture);
         }
     }
 
